fb65d39f11d2f0191021cdcdff431863e5ca1086
KaraKiz <uemuehanay@gmail.com>
2024-10-30 16:13:12 +0100

    Frontend: tests

---
+++ Frontend-Module/src/tests/HomeTests/NavBar.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import NavBar from '../../components/Home/HomeComponents/NavBar';
+import {useNavigateTo} from '../../services/designFunctions';
+
+jest.mock('../../services/designFunctions', () => ({
+  useNavigateTo: jest.fn(),
+}));
+
+describe('NavBar Component', () => {
+  const navigateMock = jest.fn();
+
+  beforeEach(() => {
+    useNavigateTo.mockReturnValue(navigateMock);
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('renders the navbar brand with correct text', () => {
+    render(<NavBar />);
+  });
+
+  it('renders the "Präferenzen bearbeiten" link', () => {
+    render(<NavBar />);
+  });
+
+  it('calls navigate function when "Präferenzen bearbeiten" link is clicked', () => {
+    render(<NavBar />);
+    const preferencesLink = screen.getByText('Präferenzen bearbeiten');
+    fireEvent.click(preferencesLink);
+    expect(navigateMock).toHaveBeenCalledWith('/changePref');
+  });
+});
