e0566cd085288fd79b877cc2b0950d71feed3867
WifiGarnele <rafael.sancho@gmx.de>
2024-10-31 18:34:07 +0100

    main.py

--- Weather-Module/main.py
+++ Weather-Module/main.py
@@ -1,1 +1,16 @@
 from weatherController import getWetter
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+app=Flask(__name__)
+CORS(app)
+
+@app.route("/weather", methods=['GET'])
+def weather():
+    ort = request.args.get('ort', type=str)
+    wetter = getWetter(ort)
+    if wetter.get("error") is None:
+        return jsonify(wetter), 200
+    else:
+        return jsonify(wetter), 400
+if __name__ == '__main__':
+    app.run(debug=True)
