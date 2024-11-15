453a854e4856530f1d0367403aa0c9e7eae89dab
WifiGarnele <rafael.sancho@gmx.de>
2024-11-15 14:35:43 +0100

    Problem behoben

--- Frontend-Module/src/components/Preferences/GeneralLayout.js
+++ Frontend-Module/src/components/Preferences/GeneralLayout.js
@@ -1,17 +1,16 @@
 import { useNavigateTo, useDeadLineManagement, useTaskManagement } from '../../utils/designFunctions';
 import React, {useEffect} from "react";
-import {getDeadlines, getPreferences, putDeadline, putPreference} from"../../services/preferencesController"
-
-
 
 function GeneralLayout({ type, step, question, component_one, class_name_one, class_name_two, component_two, nextRoute, isTutorialCompleted, isComponentTwoEmpty }) {
     const navigate = useNavigateTo();
+    const deadlineManagement = useDeadLineManagement();
+    const taskManagement = useTaskManagement();
     useEffect(async () => { //At the first rendering of the component
         const userId=null;                 //the objects are retrieved from the server
         if (type == "deadline") {
-            await useDeadLineManagement().getDeadlinesFromServer(userId)
+            await deadlineManagement.getDeadlinesFromServer(userId)
         }else if(type=="interest"){
-            await useTaskManagement().getTasksFromServer(userId)
+            await taskManagement.getTasksFromServer(userId)
         }
     }, []);
     const handleClick = async () => {
@@ -22,10 +21,10 @@ function GeneralLayout({ type, step, question, component_one, class_name_one, cl
   // Wähle die Funktion basierend auf dem type
   switch (type) {
     case 'deadline':
-      await useDeadLineManagement().submit(userId)
+      await deadlineManagement.submit(userId)
       break;
     case 'interest':
-      await useTaskManagement().submit(userId)
+      await taskManagement.submit(userId)
       break;
     // Füge hier bei Bedarf weitere Typen hinzu
     default:
